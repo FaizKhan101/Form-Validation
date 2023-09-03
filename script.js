97c0cc848f9b23251309e37b7ccfda6f2b90138c
Faiz <faizkhan8225@gmail.com>
2023-09-03 17:25:09 +0530

    Form validation via javascript

--- script.js
+++ script.js
@@ -5,16 +5,53 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
+
 function validateForm() {
   isValid = form.checkValidity();
-  message.textContent = "Please fill out all field!";
-  message.style.color = "red";
+  if (!isValid) {
+    message.textContent = "Please fill out all field!";
+    message.style.color = "red";
+    return;
+  }
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.style.color = "red";
+    message.textContent = "Make sure passwords match!";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+
+  console.log(user);
 }
 
 function processFormData(event) {
   event.preventDefault();
 
   validateForm();
+
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
 }
 
 form.addEventListener("submit", processFormData);
